1d3cfb703195d0f23aff35149203230ee91f3e54
Burak <burakgumusfb@gmail.com>
2023-07-10 23:22:03 +0300

    console app

--- blackjack-backend/src/modules/blackjack/dto/draw-card.dto.ts
+++
@@ -1,9 +0,0 @@
-import { IsNotEmpty } from 'class-validator';
-
-export class DrawCardDto {
-  @IsNotEmpty()
-  playerName: string;
-
-  @IsNotEmpty()
-  action: string;
-}
--- src/models/player.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const schema = new mongoose.Schema({
-    name: { type: String, required: true },
-    delay: { type: Number }
-});
-
-const Player = mongoose.model('players', schema)
-module.exports = Player;
